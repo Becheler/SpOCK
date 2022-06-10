c15a5de62b2af628a5176f20959efea56981f719
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-06-09 22:09:14 -0400

    fix: should work

--- include/spock_lib/gravity/perfect_sphere.h
+++ include/spock_lib/gravity/perfect_sphere.h
@@ -5,7 +5,7 @@
 #ifndef __PERFECT_SPHERE_GRAVITY_CLASS_H_INCLUDED__
 #define __PERFECT_SPHERE_GRAVITY_CLASS_H_INCLUDED__
 
-#include "../physic_constant.hpp"
+#include "../physic_constants.hpp"
 
 #include <units/isq/si/length.h>       // units::isq::si::metre
 #include <units/quantity_point_kind.h> // to define altitude
