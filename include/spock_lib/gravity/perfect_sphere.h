bc75bf65924078cf6e3bf738add0956d5a4ddc74
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-06-12 00:33:41 -0400

    fix: typo

--- include/spock_lib/gravity/perfect_sphere.h
+++ include/spock_lib/gravity/perfect_sphere.h
@@ -41,7 +41,7 @@ namespace spock::gravity::model
       constexpr auto g_0 = planet_type::g_0;
       constexpr auto radius = planet_type::r;
       constexpr units::isq::si::length<metre, double> h = units::quantity_point_kind_cast<metre>(alt);
-      constexpr units::isq::si::length<metre, double> r = units::quantity_kind_cast<metre>(r);
+      constexpr units::isq::si::length<metre, double> r = units::quantity_kind_cast<metre>(radius);
       return g_0 * (r/(r + h)) * (r/(r + h));
     }
   }; // end class perfect_sphere
