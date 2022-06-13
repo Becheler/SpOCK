6458b6f3b5d4c6660ba13d725967e3a22c852669
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-06-13 02:06:19 -0400

    fix: mp units seems to compile

--- include/spock_lib/gravity/perfect_sphere.h
+++ include/spock_lib/gravity/perfect_sphere.h
@@ -38,11 +38,10 @@ namespace spock::gravity::model
     static constexpr units::isq::Acceleration auto acceleration_at(const altitude& alt)
     {
       using units::isq::si::metre;
-      using units::isq::si::length;
+      using units::isq::si::dim_length;
       constexpr auto g_0 = planet_type::g_0;
-      constexpr auto radius = planet_type::r;
-      constexpr length<metre, double> h = units::quantity_point_kind_cast<metre>(alt);
-      constexpr length<metre, double> r = units::quantity_kind_cast<metre>(radius);
+      constexpr auto r = planet_type::r.common();
+      auto h = alt.relative().common();
       return g_0 * (r/(r + h)) * (r/(r + h));
     }
   }; // end class perfect_sphere
